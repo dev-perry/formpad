b55bd406e67a54477be735e734e8d77c1f4906ae
Perry Asibey-Bonsu <40182399+dev-perry@users.noreply.github.com>
2023-02-12 18:11:28 -0500

    Input model and templates updated to id attribute

--- formpad/inputs/base.py
+++ formpad/inputs/base.py
@@ -1,8 +1,15 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 from formpad.attributes import Attribute
+import re
 
 class Input(BaseModel):
     type: str
-    name: str
+    id: str
     template_file: str
     attributes: Attribute
+
+    @validator('id')
+    def id_is_kebab_case(cls, v):
+        if not re.match(r'^[a-z0-9]+(-[a-z0-9]+)*$', v):
+            raise ValueError('id must be in kebab-case')
+        return v
